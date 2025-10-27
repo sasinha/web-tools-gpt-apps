848122ad486f7db00bd101e024ccf5b9d7058705
Sanchit Sinha <sinhasunny1979@gmail.com>
2025-10-26 20:33:14 -0700

    Moved unreveal to next/prev for swipes

--- src/my-trivia/flashcard-viewer.jsx
+++ src/my-trivia/flashcard-viewer.jsx
@@ -7,19 +7,23 @@ export const FlashcardViewer = ({ flashcards, title = '' }) => {
     const [revealed, setRevealed] = useState(false);
     const [fi, setFi] = useState(0);
 
-    const next = () => setFi((cFi) => (cFi < flashcards.length - 1 ? cFi + 1 : cFi));
+    const next = () => {
+        setFi((cFi) => (cFi < flashcards.length - 1 ? cFi + 1 : cFi));
+        setRevealed(false);
+    };
 
-    const prev = () => setFi((cFi) => (cFi > 0 ? cFi - 1 : cFi));
+    const prev = () => {
+        setFi((cFi) => (cFi > 0 ? cFi - 1 : cFi));
+        setRevealed(false);
+    };
 
     eventListener('keydown', (e) => {
         if (e.code === 'Space') {
             setRevealed((currRev) => !currRev);
         } else if (e.code === 'ArrowRight') {
             next();
-            setRevealed(false);
         } else if (e.code === 'ArrowLeft') {
             prev();
-            setRevealed(false);
         }
     });
 
